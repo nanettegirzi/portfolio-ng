587c7574a90776ba4160ef65eeea7a2860fd97d6
Nanette Girzi <ngirzi@gmail.com>
2018-05-11 14:07:44 -0700

    add typed animation

--- js/script.js
+++ js/script.js
@@ -4,7 +4,16 @@ $(document).ready(function() {
     animation: 'fade',
     play: 4000,
     pagination: false,
-
   });
+
+  var typed = new Typed(".typed", {
+    strings: ["Web Developer." ],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false
+  });
+
+
 
 });
