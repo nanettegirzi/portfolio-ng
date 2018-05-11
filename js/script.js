e7b58761611f416a5f1000071bd22e284539acc9
Nanette Girzi <ngirzi@gmail.com>
2018-05-11 15:12:31 -0700

    add about section

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 
-  $('#slides').superslides({
+  $('#slides').superslides ({
     animation: 'fade',
     play: 4000,
     pagination: false,
   });
 
   var typed = new Typed(".typed", {
-    strings: ["Web Developer." ],
+    strings: ["Web Developer.", "Student.", "Adventurer."],
     typeSpeed: 70,
     loop: true,
     startDelay: 1000,
